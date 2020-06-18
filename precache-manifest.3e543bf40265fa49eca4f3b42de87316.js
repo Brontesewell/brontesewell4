eec897ee16221608d4e2620eff214cef4d25fab3
Brontesewell <brontsewell7@gmail.com>
2020-06-18 10:41:32 -0700

    Updates

---
+++ precache-manifest.3e543bf40265fa49eca4f3b42de87316.js
@@ -0,0 +1,214 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a471f167ac9061bd66d407abd40d1a7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c341af208a73f7e1721f",
+    "url": "./static/css/main.54757e4c.chunk.css"
+  },
+  {
+    "revision": "d010f3194e19e6fc19d2",
+    "url": "./static/js/2.a164a87b.chunk.js"
+  },
+  {
+    "revision": "0e2c12bd57e7fbc313db9e21bc0ba939",
+    "url": "./static/js/2.a164a87b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c341af208a73f7e1721f",
+    "url": "./static/js/main.10a860e1.chunk.js"
+  },
+  {
+    "revision": "e8cafcf8102cbfb7fcbd",
+    "url": "./static/js/runtime-main.973e38ae.js"
+  },
+  {
+    "revision": "74d8a826be558268a8bb007df4f93033",
+    "url": "./static/media/111.74d8a826.jpeg"
+  },
+  {
+    "revision": "eff73f401d27e300c328bcf46dfffd99",
+    "url": "./static/media/Facetune_16-06-2020-08-58-25.eff73f40.JPG"
+  },
+  {
+    "revision": "ebd666e43a40c70f8e9c13d1c69eec1c",
+    "url": "./static/media/IMG_0140.ebd666e4.jpg"
+  },
+  {
+    "revision": "dad87dc84f032120dbef8e9594df9c96",
+    "url": "./static/media/IMG_2115.dad87dc8.jpeg"
+  },
+  {
+    "revision": "6e3393328c7ca26226c37ba53654d277",
+    "url": "./static/media/IMG_2229.6e339332.jpeg"
+  },
+  {
+    "revision": "adcceb2e50f9d188f1a5583a9be1546a",
+    "url": "./static/media/IMG_4421.adcceb2e.jpg"
+  },
+  {
+    "revision": "2d2d927d44e29bbb9673d10062348ab7",
+    "url": "./static/media/IMG_5361.2d2d927d.PNG"
+  },
+  {
+    "revision": "e78faa9a1c08b7ec3985c37d1826cb63",
+    "url": "./static/media/IMG_5370.e78faa9a.JPG"
+  },
+  {
+    "revision": "ff796c4c5d2881da538c6df2195541fe",
+    "url": "./static/media/IMG_5837.ff796c4c.JPG"
+  },
+  {
+    "revision": "69341037ef2a56d9120b8c8a5e07f2e9",
+    "url": "./static/media/currentimg.69341037.png"
+  },
+  {
+    "revision": "036425ee7b4364cbe953bdffc05b15be",
+    "url": "./static/media/horse.036425ee.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5605c8aab2e2576875828b428848b4d1",
+    "url": "./static/media/me.5605c8aa.jpg"
+  },
+  {
+    "revision": "8a12fdf88bfb4a9f072c23ee8c18b7af",
+    "url": "./static/media/photo1.8a12fdf8.PNG"
+  },
+  {
+    "revision": "d618218499ea69ee8f3e50e6b6074fb2",
+    "url": "./static/media/photo123.d6182184.png"
+  },
+  {
+    "revision": "d19e4686082f3a4de1b1d7ccfa155895",
+    "url": "./static/media/photo212.d19e4686.JPG"
+  },
+  {
+    "revision": "06ffc71f896a43593be9784a596bd50b",
+    "url": "./static/media/photo3.06ffc71f.png"
+  },
+  {
+    "revision": "8b36e320ca2ed3f1d85166b4b9fe4bc0",
+    "url": "./static/media/photo35.8b36e320.jpeg"
+  },
+  {
+    "revision": "277917c2f21a29bf7408643d5ef33ad6",
+    "url": "./static/media/photo37.277917c2.JPG"
+  },
+  {
+    "revision": "4766d7f50afadcaf7d9f4c88c197a789",
+    "url": "./static/media/photo37.4766d7f5.PNG"
+  },
+  {
+    "revision": "6da73615b62f35ef17d7d46587bb2399",
+    "url": "./static/media/photo382.6da73615.JPG"
+  },
+  {
+    "revision": "257cc613b45a7913ea9d0fb4c23d853f",
+    "url": "./static/media/photo39.257cc613.JPG"
+  },
+  {
+    "revision": "2dddcc1077f4c67512c9f0a32a5f91bd",
+    "url": "./static/media/photo4.2dddcc10.PNG"
+  },
+  {
+    "revision": "be381cfb9fc5375506b5cf9af3e597f3",
+    "url": "./static/media/photo44.be381cfb.JPG"
+  },
+  {
+    "revision": "e87592b5a6a7f366c2f1af6257f85a69",
+    "url": "./static/media/photo46.e87592b5.JPG"
+  },
+  {
+    "revision": "9168f49d9d658b8ea3d37b1a2025ad51",
+    "url": "./static/media/photo49.9168f49d.JPG"
+  },
+  {
+    "revision": "53c1076feb7b902fbcdd92e4ff908855",
+    "url": "./static/media/photo5.53c1076f.png"
+  },
+  {
+    "revision": "80bd7bd2a47ebb47702b89ce00e29156",
+    "url": "./static/media/photo55.80bd7bd2.png"
+  },
+  {
+    "revision": "878e10d0b67a828478b2686c7f699326",
+    "url": "./static/media/photo6.878e10d0.JPG"
+  },
+  {
+    "revision": "b282d5c1489a31e6092e5167aeb1a03a",
+    "url": "./static/media/photo7.b282d5c1.JPG"
+  },
+  {
+    "revision": "b0019933404c864b9b7f1adf3f6474d6",
+    "url": "./static/media/photo7373.b0019933.JPG"
+  },
+  {
+    "revision": "84e8bf5901fb0820c89547cc1ecb1038",
+    "url": "./static/media/photo88.84e8bf59.png"
+  },
+  {
+    "revision": "71216ef9585e974ec58a2bcf38b25802",
+    "url": "./static/media/photo964.71216ef9.JPG"
+  },
+  {
+    "revision": "83abe5299e942bc6071fe462c588b2e1",
+    "url": "./static/media/photo97.83abe529.JPG"
+  },
+  {
+    "revision": "f4f80e43550f7a309ebd2e65279ad219",
+    "url": "./static/media/sweetie.f4f80e43.jpg"
+  },
+  {
+    "revision": "1921602fdc129527b3fd85b5b968ea0d",
+    "url": "./static/media/website1.1921602f.png"
+  },
+  {
+    "revision": "5adf93719e79af34f95f607c986592ac",
+    "url": "./static/media/website10.5adf9371.png"
+  },
+  {
+    "revision": "7b80af36cd8f1ee3595b10ecb6a83cef",
+    "url": "./static/media/website11.7b80af36.png"
+  },
+  {
+    "revision": "bb6e3a8363905f0b932c85ebc034f5e9",
+    "url": "./static/media/website12.bb6e3a83.png"
+  },
+  {
+    "revision": "bd31f6a6efd185b938f624c6706eb901",
+    "url": "./static/media/website2.bd31f6a6.png"
+  },
+  {
+    "revision": "5c1d365b661a68bfbe27388f0db47ce0",
+    "url": "./static/media/website3.5c1d365b.png"
+  },
+  {
+    "revision": "5a8c71f5ce5b96596e151bb1c9979def",
+    "url": "./static/media/website4.5a8c71f5.png"
+  },
+  {
+    "revision": "5cfc7b934c46b335107d745f83aa93ec",
+    "url": "./static/media/website5.5cfc7b93.png"
+  },
+  {
+    "revision": "55e7fc5cf6cf3b1ed91dec9c562bf47e",
+    "url": "./static/media/website6.55e7fc5c.png"
+  },
+  {
+    "revision": "612b06ea7358d7c9e18e73476f391660",
+    "url": "./static/media/website7.612b06ea.png"
+  },
+  {
+    "revision": "ca5e178454288b45fce8d7f0b0a01146",
+    "url": "./static/media/website8.ca5e1784.png"
+  },
+  {
+    "revision": "0e945aea67b39bd2d7a0c368b2b37566",
+    "url": "./static/media/website9.0e945aea.png"
+  }
+]);
